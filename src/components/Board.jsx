4af8c1e0e36ea0c2c39f735ac6acee5d1f437ed4
KiickS2 <jvmasz1@gmail.com>
2024-03-06 15:40:59 -0300

    Building the Game.jsx, Board.jsx and Card.jsx components

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Card from './Card'
 
-const Board = () => {
+const Board = ({cards}) => {
 
   return (
     <div className='board'>
-        <Card />
+        {cards.map((card) => (
+            <Card key={card.index} card={card}/>  
+        ))}
+
     </div>
   )
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Board from './Board'
+
+const suffleArray = (array) => {
+
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+const generateCards = () => {
+
+    const values = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+    const cards = values.map((value) => ({
+        value,
+        isFlipped: false
+    }))
+
+    const duplicatedCards = cards.concat(cards).map((card, index) => ({...card, index}));
+
+    return suffleArray(duplicatedCards)
+}
 
 const Game = () => {
 
@@ -7,31 +31,14 @@ const Game = () => {
     const [flippedCards, setFlippedCards] = useState([]);
     const [chance, setChance] = useState(6);
 
-    const suffleArray = (array) => {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random * (i + 1));
-
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-
-    const generateCards = () => {
-
-        const values = ["A", "B", "C", "D", "E", "F", "G", "H"];
-
-        const cards = values.map((value) => ({
-            value,
-            isFlipped: false
-        }))
-
-        const duplicatedCards = cards.map((card, index) => ({...card, index}));
-
-        return suffleArray(duplicatedCards)
-    }
+    const result = cards.filter((card) => card.isFlipped).length;
 
     return (
         <div className='game'>
-            <Board />
+            <Board cards={cards}/>
+            {chance === 0 ?
+                (<p>Suas tentativas acabaram.</p>) : result === cards.length ?
+                (<h2>Parabéns, você ganhou!</h2>) : (<p>Você possui {chance} tentativas.</p>)}
         </div>
     )
 }
